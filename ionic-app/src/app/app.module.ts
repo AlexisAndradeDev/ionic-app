15e70973c1ad6cde6014adee829772addfc5a6b1
Alexis Andrade <alexis.martinez.contacto@gmail.com>
2024-04-27 02:26:07 -0600

    Crea menú lateral
    * WIP: Crea interfaces Rutina y Ejercicio

--- ionic-app/src/app/app.module.ts
+++ ionic-app/src/app/app.module.ts
@@ -7,10 +7,19 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+    BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    MatSidenavModule, MatToolbarModule, MatIconModule, MatButtonModule
+  ],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ ionic-app/src/app/interface/ejercicio.ts
@@ -0,0 +1,3 @@
+export interface Ejercicio {
+    name: string;
+}
