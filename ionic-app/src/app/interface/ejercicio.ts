69ecd6a96161342a6e0c4f1900a1f863ae8eb60a
Alexis Andrade <alexis.martinez.contacto@gmail.com>
2024-05-17 13:32:13 -0600

    Actualiza interfaces

--- ionic-app/src/app/interface/ejercicio.ts
+++ ionic-app/src/app/interface/ejercicio.ts
@@ -1,3 +1,6 @@
 export interface Ejercicio {
     name: string;
+    series: number;
+    repeticiones: number;
+    descanso_en_segundos: number;
 }
--- ionic-app/src/app/interface/rutina.ts
+++ ionic-app/src/app/interface/rutina.ts
@@ -3,4 +3,5 @@ import { Ejercicio } from "./ejercicio";
 export interface Rutina {
     name: string;
     ejercicios?: Ejercicio[];
+    fecha_creacion: Date;
 }
