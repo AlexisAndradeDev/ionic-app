f04095a8d3bc18cd1192ec351b69d23e34dfc970
Brayan Martinez <142542930+BrayanMtz314@users.noreply.github.com>
2024-05-17 16:38:21 -0600

    WIP

--- ionic-app/src/app/rutinas/crear-rutinas/crear-rutinas.page.ts
+++ ionic-app/src/app/rutinas/crear-rutinas/crear-rutinas.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Rutina } from 'src/app/interface/rutina';
+import { collection, setDoc, doc } from 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from 'firebase/app';
 
 @Component({
   selector: 'app-crear-rutinas',
@@ -8,11 +12,22 @@ import { Rutina } from 'src/app/interface/rutina';
 })
 export class CrearRutinasPage implements OnInit {
   fechaActual: string;
+  firebaseConfig = {
+    apiKey: "AIzaSyCf7OjEvoRpW9RHmE0r7NvaG0wWaXO3Lko",
+    authDomain: "ionic-app-6a9e5.firebaseapp.com",
+    projectId: "ionic-app-6a9e5",
+    storageBucket: "ionic-app-6a9e5.appspot.com",
+    messagingSenderId: "406470949843",
+    appId: "1:406470949843:web:099bd3034b5fc40ec50d26"
+  };
 
-  constructor() {
+  app = initializeApp(this.firebaseConfig);
+
+  db = getFirestore(this.app);
+
+  constructor(private router: Router) {
     this.fechaActual = this.obtenerFechaActual();
   }
-
 
   ngOnInit() {}
 
@@ -23,7 +38,6 @@ export class CrearRutinasPage implements OnInit {
     const dia = fecha.getDate().toString().padStart(2, '0');
     return `${dia}/${mes}/${año}`;
   }
-  
 
   rutina: Rutina = {
     nombre: '',
@@ -44,8 +58,12 @@ export class CrearRutinasPage implements OnInit {
     this.rutina.ejercicios.splice(index, 1);
   }
 
-  guardarRutina() {
+  async guardarRutina() {
+    this.rutina.fecha_creacion = this.fechaActual;
+    const nuevaRutina = doc(collection(this.db, 'rutina'));
+    await setDoc(nuevaRutina, this.rutina);
     console.log('Rutina guardada:', this.rutina);
-    // Aquí puedes agregar la lógica para guardar la rutina, como hacer una petición HTTP a tu backend
+    // Aquí puedes agregar la lógica para guardar la rutina, como hacer una petición HTTP a tu backend\
+    this.router.navigate(['/rutinas']);
   }
 }
