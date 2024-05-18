6581195fe8dabc8513b78d400cf84c0023aa9a0c
AlexisAndradeDev <alexis.martinez.contacto@gmail.com>
2024-05-18 05:12:24 -0600

    Deploy en Android
    * La aplicación en Android no puede utilizar las rutinas comunitarias, pues necesitaría correr localmente el json-server

--- ionic-app/capacitor.config.ts
+++ ionic-app/capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'ionic-app',
+  appId: 'nautica.gym',
+  appName: 'Nautica Gym',
   webDir: 'www',
   server: {
     androidScheme: 'https'
