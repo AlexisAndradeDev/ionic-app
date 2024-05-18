a7cb2a3f0391f62049732564835b29d140cd6a8a
Brayan Martinez <142542930+BrayanMtz314@users.noreply.github.com>
2024-05-17 18:59:24 -0600

    getAllRutinas

--- ionic-app/src/app/service/rutinas.service.ts
+++ ionic-app/src/app/service/rutinas.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { getFirestore, collection, addDoc, getDocs, serverTimestamp, Timestamp } from 'firebase/firestore'
+import { getFirestore, collection, addDoc, getDocs, serverTimestamp, Timestamp, collectionGroup } from 'firebase/firestore'
 import { AuthService } from './auth.service';
 import { Rutina } from '../interface/rutina';
+import { Observable, from, map } from 'rxjs';
 
 
 @Injectable({
@@ -32,4 +33,22 @@ export class RutinasService {
     );
     console.log("Documento escrito con id: " + docRef.id);
   }
+
+  getAllRutinas(): Observable<Rutina[]> {
+    const rutinaCollection = collection(this.db, 'rutina');
+    return from(getDocs(rutinaCollection)).pipe(
+      map((querySnapshot: { docs: any[]; }) => {
+        return querySnapshot.docs.map(doc => {
+          const data = doc.data();
+          return {
+            nombre: data.nombre,
+            ejercicios: data.ejercicios,
+            fecha_creacion: (data.fecha_creacion as Timestamp).toDate().toString(),
+          } as unknown as Rutina;
+        });
+      })
+    );
+  }
+
+
 }
